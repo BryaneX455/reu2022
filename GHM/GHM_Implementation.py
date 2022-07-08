e3dcaab7175b83620fb9a83ca71484c2b0150d3a
BryaneX455 <55569826+BryaneX455@users.noreply.github.com>
2022-07-08 10:36:37 -0500

    Add files via upload

--- GHM/GHM_Implementation.py
+++ GHM/GHM_Implementation.py
@@ -17,11 +17,19 @@ import matplotlib.pyplot as plt
 """ Parameter: Graph G, Phase State S, Period k,Iteration Number ItNum """
 def GHM(G,S,kap,ItNum):
     St = S
-    SN = S
+    SN = np.zeros(G.number_nodes)
+    for i in range(len(S)):
+        SN[i] = S[i]
     for i in range(ItNum):
+        print(St)
+        print('this is the state,\n')
+        print(S)
+        print('this is previous S')
         if i!=0:
             S = SN
             St = np.vstack((St,SN))
+            print(SN)
+            print('this is SN')
         for j in range(G.number_nodes):
             Onein = False
             NeighbNum = len(G.neighbors(G.vertices[j]))
@@ -30,31 +38,25 @@ def GHM(G,S,kap,ItNum):
             NeighbState = np.zeros(NeighbNum)
             for k in range(NeighbNum):
                 NeighbState[k] = S[int(NeighbList[k])-1]  
-            print(NeighbState)    
-            print(SN)
-            print(S)
+
             if 1 in NeighbState:
                 Onein = True
-            print(Onein)
-            if SN[j] == 0 and (not Onein):
+            if S[j] == 0 and (not Onein):
                 SN[j] = 0
-            elif SN[j] == 0 and Onein:
+            elif S[j] == 0 and Onein:
                 SN[j] = 1 % kap
             else:
-                if (SN[j] + 1) % kap == 0:
+                if (S[j] + 1) % kap == 0:
                     SN[j]=0
                 else:       
                     SN[j] = (SN[j] + 1) % kap
-            print(SN)
-            print(St)
-            print('\n')
-    
+
     return St     
    
         
         
 """Synchronizing example"""
-edgelist = [['1','2'],['2','3'],['3','4'],['1','3'],['1','5'],['1','6'],['1','7'],['2','8'],['2','9'],['1','9'],['2','4'],['2','5'],['3','6'],\
+edgelist = [['1','2'],['2','3'],['3','4'],['1','3'],['1','5'],['1','6'],['1','7'],['1','8'],['2','8'],['2','9'],['1','9'],['2','4'],['2','5'],['3','6'],\
             ['3','4'],['3','5'],['3','6'],['3','8'],['3','9'],['4','5'],['4','6'],['4','7'],['4','9'],['5','6'],['5','7'],['6','8']]
 G1 = NNT()
 G1.add_edges(edgelist)
@@ -63,7 +65,7 @@ G1G.add_edges_from(edgelist)
 plt.figure(1)
 nx.draw(G1G, with_labels=True, font_weight='bold')
 
-State = GHM(G1,[0,2,1,3,2,1,2,0,3],7,7)
+State = GHM(G1,[0,2,1,3,2,1,2,0,3],7,9)
 G1map = pd.DataFrame(State)
 plt.figure(2)
 sb.heatmap(G1map)
