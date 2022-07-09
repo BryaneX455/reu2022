cc5d345ade1793cb1f8e5563b913f4d84fda0cd5
BryaneX455 <55569826+BryaneX455@users.noreply.github.com>
2022-07-09 13:30:53 -0500

    GHM1D2D

--- GHM/GHM_Implementation.py
+++ GHM/GHM_Implementation.py
@@ -15,25 +15,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 """ Parameter: Graph G, Phase State S, Period k,Iteration Number ItNum """
-def GHM(G,S,kap,ItNum):
+def GHMArr(G,S,kap,ItNum):
     St = S
-    SN = np.zeros(G.number_nodes)
+    SN = np.zeros(G.number_of_nodes())
+    NodeNum = G.number_of_nodes()
     for i in range(len(S)):
         SN[i] = S[i]
     for i in range(ItNum):
-        print(St)
-        print('this is the state,\n')
-        print(S)
-        print('this is previous S')
         if i!=0:
             S = SN
             St = np.vstack((St,SN))
-            print(SN)
-            print('this is SN')
-        for j in range(G.number_nodes):
+        for j in range(NodeNum):
             Onein = False
-            NeighbNum = len(G.neighbors(G.vertices[j]))
-            NeighbSet = G.neighbors(G.vertices[j])
+            NeighbNum = len(list(G.neighbors(list(G.nodes)[j])))
+            NeighbSet = G.neighbors(list(G.nodes)[j])
             NeighbList = list(NeighbSet)
             NeighbState = np.zeros(NeighbNum)
             for k in range(NeighbNum):
@@ -50,24 +45,27 @@ def GHM(G,S,kap,ItNum):
                     SN[j]=0
                 else:       
                     SN[j] = (SN[j] + 1) % kap
+    
+    PhaseState = pd.DataFrame(St)
+    
 
-    return St     
+    return sb.heatmap(PhaseState, cbar=False, cmap='viridis')     
    
         
         
 """Synchronizing example"""
 edgelist = [['1','2'],['2','3'],['3','4'],['1','3'],['1','5'],['1','6'],['1','7'],['1','8'],['2','8'],['2','9'],['1','9'],['2','4'],['2','5'],['3','6'],\
             ['3','4'],['3','5'],['3','6'],['3','8'],['3','9'],['4','5'],['4','6'],['4','7'],['4','9'],['5','6'],['5','7'],['6','8']]
-G1 = NNT()
-G1.add_edges(edgelist)
-G1G = nx.Graph()
-G1G.add_edges_from(edgelist)
+G1 = nx.Graph()
+G1.add_edges_from(edgelist)
 plt.figure(1)
-nx.draw(G1G, with_labels=True, font_weight='bold')
-
-State = GHM(G1,[0,2,1,3,2,1,2,0,3],7,9)
-G1map = pd.DataFrame(State)
+nx.draw(G1, with_labels=True, font_weight='bold')
 plt.figure(2)
-sb.heatmap(G1map)
+s = np.random.randint(7,size = 1*9)
+GHMArr(G1, s, 12, 15);
 
+
+Grand2DArr = nx.grid_2d_graph(10, 10)
+s = np.random.randint(4, size=10*10)
+GHMArr(Grand2DArr, s, 4, 100);
 
