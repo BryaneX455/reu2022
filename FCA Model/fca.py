2fd55077c60ea6c7c86f71d8823c4a8f500748ac
AGoyal0512 <agoyal25@wisc.edu>
2022-07-10 12:30:59 -0500

    minor update

--- FCA Model/fca.py
+++ FCA Model/fca.py
@@ -1,3 +1,30 @@
+import networkx as nx
+
+import time
+import random
+import numpy as np
+import pandas as pd
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 """
 Implementing a class for FCA
 """
+
+class FCA:
+    """
+    """
+
+    def __init__(self):
+        return
+
+    def iterate_fca():
+        return
+
+    def plt_fca():
+        return
+
+    def animate_fca():
+        return
