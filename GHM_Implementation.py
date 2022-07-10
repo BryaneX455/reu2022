7411b2b9107a538d23b3e0da18b9b844b720eecd
BryaneX455 <55569826+BryaneX455@users.noreply.github.com>
2022-07-09 23:30:52 -0500

    Add files via upload

---
+++ GHM_Implementation.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 20:55:03 2022
+
+@author: Zihong Xu
+"""
+
+
+
+from NNetwork.NNetwork import NNetwork as NNT
+import numpy as np
+import pandas   as pd
+import seaborn as sb
+import networkx as nx
+import matplotlib.pyplot as plt
+
+""" Parameter: Graph G, Phase State S, Period k,Iteration Number ItNum """
+def GHMArr(G,S,kap,ItNum):
+    St = S
+    SN = np.zeros(G.number_of_nodes())
+    NodeNum = G.number_of_nodes()
+    for i in range(len(S)):
+        SN[i] = S[i]
+    for i in range(ItNum):
+        if i!=0:
+            S = SN
+            St = np.vstack((St,SN))
+        for j in range(NodeNum):
+            Onein = False
+            NeighbNum = len(list(G.neighbors(list(G.nodes)[j])))
+            NeighbSet = G.neighbors(list(G.nodes)[j])
+            NeighbList = list(NeighbSet)
+            NeighbState = np.zeros(NeighbNum)
+            for k in range(NeighbNum):
+                NeighbState[k] = S[int(NeighbList[k])-1]  
+
+            if 1 in NeighbState:
+                Onein = True
+            if S[j] == 0 and (not Onein):
+                SN[j] = 0
+            elif S[j] == 0 and Onein:
+                SN[j] = 1 % kap
+            else:
+                if (S[j] + 1) % kap == 0:
+                    
+                    SN[j]=0
+                else:       
+                    SN[j] = (SN[j] + 1) % kap
+    
+    PhaseState = pd.DataFrame(St)
+    
+
+    return sb.heatmap(PhaseState, cbar=False, cmap='viridis')     
+   
+        
+        
+"""Synchronizing example"""
+edgelist = [['1','2'],['2','3'],['3','4'],['1','3'],['1','5'],['1','6'],['1','7'],['1','8'],['2','8'],['2','9'],['1','9'],['2','4'],['2','5'],['3','6'],\
+            ['3','4'],['3','5'],['3','6'],['3','8'],['3','9'],['4','5'],['4','6'],['4','7'],['4','9'],['5','6'],['5','7'],['6','8']]
+G1 = nx.Graph()
+G1.add_edges_from(edgelist)
+plt.figure(1)
+nx.draw(G1, with_labels=True, font_weight='bold')
+plt.figure(2)
+s = np.random.randint(7,size = 1*9)
+GHMArr(G1, s, 12, 15);
+
+
+def GHMGridToArr(G,S,kap,ItNum):
+    GArr = list(np.zeros(G.number_of_nodes()))
+    RowNum = 0
+    for i in range(G.number_of_nodes()):
+        if list(Grand2DArr.nodes)[i][0] == 0:
+            RowNum += 1
+    ColNum = int(G.number_of_nodes()/RowNum)
+    for i in range(RowNum):
+        for j in range(ColNum):
+            GArr[int(i)*ColNum + int(j)] = int(i)*ColNum + int(j)
+    print(GArr)
+    St = S
+    SN = np.zeros(G.number_of_nodes())
+    NodeNum = G.number_of_nodes()
+    for i in range(len(S)):
+        SN[i] = S[i]
+    for i in range(ItNum):
+        if i!=0:
+            S = SN
+            St = np.vstack((St,SN))
+        for j in range(NodeNum):
+            Onein = False
+            NeighbNum2D = len(list(G.neighbors(list(G.nodes)[j])))
+            NeighbSet2D = G.neighbors(list(G.nodes)[j])
+            NeighbList2D = list(NeighbSet2D)
+            print(NeighbList2D)
+            NeighbPos = list(np.zeros(len(NeighbList2D)))
+            for k in range(len(NeighbList2D)):
+                NeighbPos[k] = NeighbList2D[k][0]*ColNum+NeighbList2D[k][1]
+            print(NeighbPos)
+            NeighbState2D = np.zeros(NeighbNum2D)
+            
+            for k in range(NeighbNum2D):
+                NeighbState2D[k] = S[int(NeighbPos[k])]  
+
+            if 1 in NeighbState2D:
+                Onein = True
+            if S[j] == 0 and (not Onein):
+                SN[j] = 0
+            elif S[j] == 0 and Onein:
+                SN[j] = 1 % kap
+            else:
+                if (S[j] + 1) % kap == 0:
+                    
+                    SN[j]=0
+                else:       
+                    SN[j] = (SN[j] + 1) % kap
+    
+    PhaseState = pd.DataFrame(St)
+    
+
+    return sb.heatmap(PhaseState, cbar=False, cmap='viridis')   
+    
+Grand2DArr = nx.grid_2d_graph(10, 10)
+
+print(len(list(Grand2DArr.nodes)[0]))
+print(Grand2DArr.number_of_nodes())
+print(Grand2DArr.size()) 
+s = np.random.randint(7, size=1*100)
+
+GHMGridToArr(Grand2DArr,s,7,10)
+
