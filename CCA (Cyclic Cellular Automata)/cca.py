429c1f40a3c2a40b4180d22c5f3e7ca44d0c1838
Binhao <47936184+13inhao@users.noreply.github.com>
2022-07-11 16:15:40 -0500

    comments added

--- CCA (Cyclic Cellular Automata)/cca.py
+++ CCA (Cyclic Cellular Automata)/cca.py
@@ -7,6 +7,20 @@ from networkx import grid_graph
 
 
 def simulate_CCA(G, init_state, kappa, threshold, iters):
+
+    """Implements the Cyclic Cellular Automata model
+
+    Args:
+        G (NetworkX Graph): Input graph to the model
+        init_state (array): Initial state
+        kappa (int): number of colors allowed
+        threshold (int): allow a cell to be consumed when the # neighbors with successor value exceeds this threshold
+        iters (int): number of iterations
+
+    Returns:
+        state_matrix: The dynamics of this CCA model with iters iterations;
+    """
+
     state_matrix, curr_state = init_state
     next_state = np.zeros(G.number_of_nodes())
 
@@ -21,8 +35,16 @@ def simulate_CCA(G, init_state, kappa, threshold, iters):
             flag = False
             count = 0
 
+            #   CCA updateing rule (adpating from Wikiepdia):
+                # The cyclic cellular automaton consists of a regular grid of cells in one or more dimensions. 
+                # The cells can take on any of n states, ranging from 0 to n-1. 
+                # The first generation starts out with random states in each of the cells. 
+                # In each subsequent generation, if a cell has a neighboring cell whose value is the successor of the cell's value, 
+                # the cell is "consumed" and takes on the succeeding value. 
+                # (note that 0 is the successor of n-1
+
             for k in range(G.number_of_nodes()):
-                # von Neumann neighborhood
+                # appliying von Neumann neighborhood (with edge directly conencted)
                 if (curr_state[k] == (curr_state[j]+1) % kappa) and (list(G.nodes)[k] in list(G.adj[list(G.nodes)[i]])):
                     count += 1
                     if count >= threshold:
