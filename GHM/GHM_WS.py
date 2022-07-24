c03348794e1c435a0d19daf75ee31bc86597974a
BryanX <55569826+BryaneX455@users.noreply.github.com>
2022-07-24 14:30:35 -0500

    GHM_Data_WS

--- GHM/GHM_WS.py
+++ GHM/GHM_WS.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jul 20 15:58:14 2022
-
 @author: Zihong Xu
 """
 import numpy as np
@@ -13,11 +12,9 @@ import matplotlib.pyplot as plt
 import random
 import csv
 
+from pathlib import Path  
 from itertools import product
 
-
-
-graph = nx.watts_strogatz_graph(12,4,0.7)
 
 def GHM(G,S,kap,ItNum):
     S = list(S)
@@ -33,46 +30,72 @@ def GHM(G,S,kap,ItNum):
             St = np.vstack((St,SN))
         for j in range(G.number_of_nodes()):
             Onein = False
+            StateZero = False
             NeighbNum = len(list(G.neighbors(list(G.nodes)[j])))
             NeighbSet = G.neighbors(list(G.nodes)[j])
             NeighbList = list(NeighbSet)
             NeighbState = list(np.zeros(NeighbNum))
             for k in range(NeighbNum):
-                NeighbState[k] = S[k]
+                NeighbState[k] = S[int(NeighbList[k])-1]
                 
 
             if 1 in NeighbState:
                 Onein = True
-            if S[j] == 0 & (not Onein):
+            if S[j] == 0:
+                StateZero = True
+            if StateZero & (not Onein):
                 SN[j] = 0
-            elif S[j] == 0 & Onein:
+            elif StateZero & Onein:
                 SN[j] = 1 % kap
             else:
                 SN[j] = (SN[j] + 1) % kap
-    Sync = False            
+    Sync = False           
     if np.all((St[ItNum-1] == 0)):
         Sync = True
     PhaseState = pd.DataFrame(St)
     sb.heatmap(PhaseState, cbar=False, cmap='viridis') 
+    
     return St, Sync 
 
-NodeNum = 10
+NodeNum = 14
 kap = 5
 knn = 4
-#s = np.random.randint(kap, size=NodeNum)
-color_list = list(product(range(0, kap), repeat=NodeNum))
-print(len(color_list))
-file = open('GHM.csv', 'w+', newline ='')
+prob = 0.65
 GHMItNum = 30
-SyncNum = 0
-Non_Sync_Num = 0
-G = nx.watts_strogatz_graph(NodeNum,knn,0.65)
-for col in color_list:
-    states, label = GHM(G, col, kap, GHMItNum)
-    print(label)
-    if label:
-        SyncNum += 1
-    else:
-        Non_Sync_Num += 1
-print(SyncNum)
-print(Non_Sync_Num)
+
+
+def sample_WS(num_samples, NodeNum, knn, prob, kap, GHMItNum):
+    df = pd.DataFrame(columns=['kappa', '# Edges', '# Nodes', 'Min Degree', 'Max Degree', 'Diameter', 'Concentrated'])
+    
+    for i in range(num_samples):
+        
+        G = nx.watts_strogatz_graph(NodeNum, knn, prob)
+        s = np.random.randint(5, size=1*NodeNum)
+        if nx.is_connected(G):
+            # Number of Edges and Nodes
+            edges = G.number_of_edges()
+            nodes = G.number_of_nodes()
+
+            # Min and Max degree
+            degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
+            dmax = max(degree_sequence)
+            dmin = min(degree_sequence)
+
+            # Diameter of graph
+            diam = nx.diameter(G)
+            
+            # Applying GHM
+            adj_mat = nx.to_numpy_array(G)
+            
+            state, label = GHM(G, s, kap, GHMItNum)
+            if label:
+                label = 1
+            else:
+                label = 0
+            df.at[len(df.index)] = [kap, edges, nodes, dmin, dmax, diam, label]
+    
+    return df
+
+SampleNum = 700
+df =  sample_WS(SampleNum, NodeNum, knn, prob, kap, GHMItNum)
+df.to_csv('GHM.csv')  
