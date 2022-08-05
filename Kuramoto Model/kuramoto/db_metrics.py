a7c077f2d203db5b6f03ae24fce1431940c0d737
AGoyal0512 <agoyal25@wisc.edu>
2022-08-05 14:23:27 -0500

    g2v comparisons

--- Kuramoto Model/kuramoto/db_metrics.py
+++ Kuramoto Model/kuramoto/db_metrics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy. sparse import csr_matrix
 import random
 
 import networkx as nx
@@ -22,6 +23,96 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+def gen_all(nodes, r, num_samples, edge_var="L", random_K=False, half_sync=False):
+    
+    df = pd.DataFrame()
+    graph_list = []
+    
+    headers = []
+    for i in range(1, nodes+1):
+        for j in range(1, r+1):
+            headers.append(f's{i}_{j}')
+    
+    for i in range(num_samples):
+        
+        df1 = pd.DataFrame(columns=['# Edges', '# Nodes', 'Min Degree', 'Max Degree', 'Diameter', 'Concentrated'])
+        temp = pd.DataFrame()
+        
+        nodes = nodes
+        
+        assert(edge_var in ["L", "H", "F"]), "The edge variance can either be \"L\" (Low), \"H\" (High), or \"F\" (Fixed)."
+        
+        if edge_var=="L":
+            neighbors = int(random.uniform(15, 20))
+            
+        elif edge_var=="H":
+            neighbors = int(random.uniform(1, 20))
+            
+        else:
+            neighbors = 15
+            
+        probability = random.uniform(0, 1)
+        G = nx.newman_watts_strogatz_graph(nodes, neighbors, probability)
+        graph_list.append(G)
+        
+        if random_K: K = random.uniform(0.5, 4.5)
+        else: K = 1.96
+            
+        
+        if nx.is_connected(G):
+            
+            # Generate adjacency matrix
+            matrix = nx.adjacency_matrix(G)
+            matrix = csr_matrix(matrix).toarray().flatten(order='C')
+            df_matrix = pd.DataFrame(matrix).T
+            
+            # Number of Edges and Nodes
+            edges = G.number_of_edges()
+            nodes = G.number_of_nodes()
+
+            # Min and Max degree
+            degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
+            dmax = max(degree_sequence)
+            dmin = min(degree_sequence)
+
+            # Diameter of graph
+            diam = nx.diameter(G)
+
+            # Applying Kuramoto
+            adj_mat = nx.to_numpy_array(G)
+
+            if half_sync:
+                natfreqs = np.repeat(2 * np.pi * 0, nodes)
+                model = Kuramoto(coupling=K, dt=0.01, T=25, natfreqs=natfreqs, half_sync=half_sync)
+            else:
+                model = Kuramoto(coupling=K, dt=0.01, T=25, n_nodes=nodes, half_sync=half_sync)
+
+            sim = model.run(adj_mat)
+            conc = int(model.concentrated)
+            
+            df1.at[len(df1.index)] = [edges, nodes, dmin, dmax, diam, conc]
+            
+            df2 = pd.DataFrame(sim[:,:r].flatten(order='C')).T
+            df2.columns = headers
+            
+            temp = pd.concat([df_matrix, df2, df1], axis=1)
+
+            df = pd.concat([df, temp], ignore_index=True)
+            
+        else:
+            graph_list.remove(G)
+            
+    g2v = Graph2Vec(wl_iterations = 5, dimensions = 16)
+    g2v.fit(graph_list)
+    embed = g2v.get_embedding()
+    df_g2v = pd.DataFrame(embed, columns=['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 
+                                          'f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16'])
+        
+    df_final = pd.concat([df_g2v, df], axis=1)
+            
+    return df_final
+        
+        
 def gen_KM(nodes, r, num_samples, edge_var="L", random_K=False, half_sync=False):
     
     df = pd.DataFrame()
@@ -263,7 +354,8 @@ def g2v_KM_nodynamics(nodes, num_samples, edge_var="L", random_K=False, half_syn
     g2v = Graph2Vec(wl_iterations = 5, dimensions = 16)
     g2v.fit(graph_list)
     embed = g2v.get_embedding()
-    df_g2v = pd.DataFrame(embed)
+    df_g2v = pd.DataFrame(embed, columns=['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 
+                                          'f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16'])
         
     df_final = pd.concat([df_g2v, df], axis=1)
     
